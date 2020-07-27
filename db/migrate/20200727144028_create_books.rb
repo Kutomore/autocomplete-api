48538c2d16067f33289fa80b6e4cbad563ea7dce
Kutomore <kutomore@gmail.com>
2020-07-27 11:43:35 -0300

    Changing docker-compose, adding rspec, factory_bot, and Book model

---
+++ db/migrate/20200727144028_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :publisher
+      t.string :genre
+      t.date :published_at
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/books.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :book do
+    title { "MyString" }
+    author { "MyString" }
+    publisher { "MyString" }
+    genre { "MyString" }
+    published_at { "2020-07-27" }
+  end
+end
